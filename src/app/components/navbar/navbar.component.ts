27b18f1d311535b35cd217c4a22b0670558e8d93
Filip Petrovski <filip.petrovski1992@gmail.com>
2026-03-02 15:51:08 +0100

    feat: add angular material to project

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'pl-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
